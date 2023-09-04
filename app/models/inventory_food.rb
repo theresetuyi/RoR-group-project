411ca77915344b9a9ca923fee3767682a568b9e5
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-09-05 00:23:36 +0430

    Solved issue

--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -2,5 +2,5 @@ class InventoryFood < ApplicationRecord
   belongs_to: inventory
   belongs_to: food
 
-  validates :quantity, greater_than_or_equal_to: 0
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
