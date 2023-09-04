98c4ad1ba50cfde0dd9d28cbe5fdb91d07d96b51
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-09-05 00:23:47 +0430

    Created columns

--- db/migrate/20230904194503_create_inventory_foods.rb
+++ db/migrate/20230904194503_create_inventory_foods.rb
@@ -1,7 +1,7 @@
 class CreateInventoryFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :inventory_foods do |t|
-
+      t.quantity :integer, default: 0
       t.timestamps
     end
   end
