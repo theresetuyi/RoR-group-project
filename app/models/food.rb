eca0beba344aeb2a355b47b9f466fbffd8515bbb
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-09-05 00:33:37 +0430

    Validation and association added

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,7 @@
 class Food < ApplicationRecord
+  has_many :inventory_foods, dependent: :destroy
+
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
