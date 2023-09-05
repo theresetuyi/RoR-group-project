2fcecc8392c95c877aa99a1fdc8791cceaf2ae5e
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-09-05 18:03:21 +0430

    Injected some raw data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user1 = User.create(name:'Ali')
+
+inventory1 = Inventory.create(user:user1, name:'first_inventory', description:'details for thie inventory')
+
+inventory2 = Inventory.create(user:user1, name:'invent_1', description:'details for this invent_1')
+inventory3 = Inventory.create(user:user1, name:'invent_2', description:'details for this invent_2')
+inventory3 = Inventory.create(user:user1, name:'invent_3', description:'details for this invent_3')
