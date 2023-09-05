e8c504fdd481e745b3eee9feaf613fa26a752697
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-09-05 15:45:56 +0430

    Removed wrong file for adding fk

--- db/migrate/20230904200756_invent_food_pk.rb
+++
@@ -1,7 +0,0 @@
-class InventFoodPk < ActiveRecord::Migration[7.0]
-  def change
-    add_foreign_key :inventories, :users
-    add_foreign_key :inventory_foods, :inventories
-    add_foreign_key :inventory_foods, :foods
-  end
-end
