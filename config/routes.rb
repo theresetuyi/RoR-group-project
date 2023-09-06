a32aa258b5c8bb8d99eecb328ad9c8a1b861e4c7
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-09-06 18:12:44 +0430

    Added routes for the foods nested with inventories

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :inventories
+  resources :inventories do
+    resources :foods do
+      resources :inventory_foods
+    end
+  end
 end
