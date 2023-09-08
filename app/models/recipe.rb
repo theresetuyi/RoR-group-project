f117947638ec302849ec5b89b43f30ec0f121451
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-09-08 23:47:07 +0430

    Added unit test

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
   has_many :recipe_foods, foreign_key: 'recipe_id', dependent: :destroy
+
+  validates :description, presence: true
+  validates :name, presence: true
 end
