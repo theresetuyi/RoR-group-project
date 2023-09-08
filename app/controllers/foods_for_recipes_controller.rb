a133d581fce2d2454b6b7ed70417f40f08ac8560
unknown <teddytiyi@gmail.com>
2023-09-08 21:45:27 +0200

    add test of recipe

--- app/controllers/foods_for_recipes_controller.rb
+++ app/controllers/foods_for_recipes_controller.rb
@@ -4,11 +4,11 @@ class FoodsForRecipesController < ApplicationController
     @food = Food.new(food_params)
 
     if @food.save
-      flash[:notice] = "Food added successfully."
+      flash[:notice] = 'Food added successfully.'
       redirect_to select_foods_recipe_path(@recipe)
     else
-      flash[:alert] = "Food creation failed."
-      redirect_back(fallback_location: select_foods_recipe_path(@recipe), alert: "Food creation failed.")
+      flash[:alert] = 'Food creation failed.'
+      redirect_back(fallback_location: select_foods_recipe_path(@recipe), alert: 'Food creation failed.')
     end
   end
 
